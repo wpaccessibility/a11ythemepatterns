4267d6bd7e397645e35625bc497c589d1734ad5f
joedolson <design@joedolson.com>
2016-01-12 14:13:11 -0600

    Update indentation

--- responsive-menu/functions.php
+++ responsive-menu/functions.php
@@ -5,6 +5,5 @@
  */
 add_action( 'wp_enqueue_scripts', 'yourtheme_scripts' );
 function yourtheme_scripts() {
- wp_enqueue_script( 'yourtheme-responsive-toggle', get_template_directory_uri() . '/js/toggle.js', array('jquery'), '20160110', true);
-
+	wp_enqueue_script( 'yourtheme-responsive-toggle', get_template_directory_uri() . '/js/toggle.js', array('jquery'), '20160110', true);
 }
